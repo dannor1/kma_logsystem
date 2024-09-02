04b1c509644b1a9ae45bfaa040788a213b9acfb4
Daniel Kyei Annor Kumi <123387192+dannor1@users.noreply.github.com>
2024-09-02 11:32:15 +0000

    Add files via upload

---
+++ default.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>K.M.A Log System</title>
+    <meta
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+      name="viewport"
+    />
+     <link
+      rel="icon"
+      href="assets/img/logo_kma.png"
+      type="image/x-icon"
+    />
+
+    <!-- Fonts and icons -->
+    <script src="assets/js/plugin/webfont/webfont.min.js"></script>
+    <script>
+      WebFont.load({
+        google: { families: ["Public Sans:300,400,500,600,700"] },
+        custom: {
+          families: [
+            "Font Awesome 5 Solid",
+            "Font Awesome 5 Regular",
+            "Font Awesome 5 Brands",
+            "simple-line-icons",
+          ],
+          urls: ["assets/css/fonts.min.css"],
+        },
+        active: function () {
+          sessionStorage.fonts = true;
+        },
+      });
+    </script>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins.min.css" />
+    <link rel="stylesheet" href="assets/css/kaiadmin.min.css" />
+
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+     <!-- <link rel="stylesheet" href="assets/css/demo.css" /> -->
+</head>
+  <body>
+    <div class="wrapper">
+      <!-- Sidebar -->
+    <?php include("backend/inc/sidebar.php"); ?>
+    <!-- End Sidebar -->
+      
+    <!-- Header -->
+    <?php include("backend/inc/navbar.php"); ?>
+    <!-- Header -->
+
+    </div>
+</body>
+<footer>
+<?php include("backend/inc/footer.php"); ?>  
+</footer>
